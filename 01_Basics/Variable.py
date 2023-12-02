38f75d8a5305ce4a57e1e9251c83cb0e457913f2
Akshat Singh <75721746+AkshatSingh-spx@users.noreply.github.com>
2023-12-02 15:26:16 +0000

    some expirement with python

---
+++ 01_Basics/Variable.py
@@ -0,0 +1,5 @@
+// Varibale is Container to use to store a value.
+const Name = input("Please Enter your Name: ")
+let greet = "Good Evening Sir, How May I assist to You!"
+let new_greet = Name + greet;
+console.log(new_greet);
