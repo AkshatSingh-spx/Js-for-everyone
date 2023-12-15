a6752014f689948c1ee627ee545e5303c47c3539
Akshat Singh <75721746+AkshatSingh-spx@users.noreply.github.com>
2023-12-15 10:06:22 +0000

    Type Conversion and Operations

---
+++ 01_Basics/OperationaandTypeconversion.js
@@ -0,0 +1,19 @@
+// Operations in Javascript
+
+// let newNumber = 18
+// console.log(newNumber);
+// console.log(typeof(newNumber));
+
+// let newObj = {}
+// console.log(typeof newObj);
+
+// let upgradeVar = String(newNumber);
+// console.log(typeof upgradeVar);
+
+// let isUserLoggedin = false
+// console.log(Number(isUserLoggedin))
+// console.log(typeof isUserLoggedin);
+
+let isUserLoggout = {}
+console.log(Boolean(isUserLoggout));
+// console.log(typeof (isUserLoggout))
