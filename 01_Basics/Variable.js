9443f424853ca288db65a5831fb60cf861f6828e
Akshat Singh <75721746+AkshatSingh-spx@users.noreply.github.com>
2023-12-15 08:57:43 +0000

    OperationsandType

--- 01_Basics/Variable.js
+++ 01_Basics/Variable.js
@@ -1,1 +1,4 @@
 // Varibale is Container to use to store a value.
+let num = 1 
+let Str = 'Hello'
+console.log(typeof num,Str);
--- 01_Basics/Variable.py
+++
@@ -1,5 +0,0 @@
-// Varibale is Container to use to store a value.
-const Name = input("Please Enter your Name: ")
-let greet = "Good Evening Sir, How May I assist to You!"
-let new_greet = Name + greet;
-console.log(new_greet);
